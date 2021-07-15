fdbd21538631bd766e28af06ed03f47e05fd0555
Thiago Simões Gonçalves <thiago.sgoncalves@pagar.me>
2021-07-15 16:50:03 -0300

    convert cask to shell

--- install-ansible-macos.sh
+++ install-ansible-macos.sh
@@ -1,4 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 brew install python
