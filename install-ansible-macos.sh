f11781980d88892e3ad53d8bda43f0c53174a6ac
Thiago Simões Gonçalves <thiago.sgoncalves@pagar.me>
2021-07-15 14:21:48 -0300

    Fix: macinstall

--- install-ansible-macos.sh
+++ install-ansible-macos.sh
@@ -1,2 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+brew install python
 sudo pip install ansible --quiet
 ansible-galaxy collection install community.general
